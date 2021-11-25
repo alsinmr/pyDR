3d72ef12fc6f3dffc152d0643e47d813020c88d8
alsinmr <51168380+alsinmr@users.noreply.github.com>
2021-11-25 08:00:01 +0100

    Check for thread termination
    We store the first instance of ListenExec thread (line 100) and see if it remains running after obtaining messages (it does not, so this approach should be OK)

---
+++ chimeraX/chimera_script00.py
@@ -0,0 +1,13 @@
+import sys
+from chimerax.core.commands import run as rc
+
+rc(session,"remotecontrol rest start port 60958")
+sys.path.append("/Users/albertsmith/Documents/GitHub/pyDR/chimeraX")
+from RemoteCMXside import CMXReceiver as CMXR
+import RemoteCMXside
+cmxr=CMXR(session,7000)
+rc(session,"ui mousemode right select")
+rc(session,"open 2kj3")
+rc(session,"~ribbon")
+rc(session,"show #1.1@N,C,CA")
+rc(session,"style ball")
