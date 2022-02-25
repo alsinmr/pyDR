8a1e81853085c46759534244b735aa996c3ebb1f
kaizumpfe <kai_zumpfe@yahoo.de>
2022-02-25 12:41:26 +0100

    updated gui

--- GUI/_Selection.py
+++ GUI/_Selection.py
@@ -1,10 +1,47 @@
 from pyDR.GUI.designer2py.selection_widget import Ui_Selection
 from pyDR.GUI.other.elements import openFileNameDialog
+import MDAnalysis as MDA
+from PyQt5.QtWidgets import QLabel, QSpacerItem, QSizePolicy, QPushButton
+from PyQt5.QtCore import Qt, QRect
 
 class Ui_Selection_final(Ui_Selection):
     def retranslateUi(self, Selection):
+        def load_pdb():
+            self.listWidget_pdbresidues.clear()
+            pdbpath = openFileNameDialog(filetypes="*.pdb")
+            self.label_pdbpath.setText(pdbpath)
+            Uni = MDA.Universe(pdbpath)
+            for res in Uni.residues:
+                self.listWidget_pdbresidues.addItem(f"{res.resname}-{res.resid} ({res.segid})")
+
         super().retranslateUi(Selection)
-        self.pushButton_loadpdb.clicked.connect( lambda: openFileNameDialog(target=self.label_pdbpath, filetypes="*.pdb"))
+        self.pushButton_loadpdb.clicked.connect(lambda: load_pdb())
         #
+        def change_label(label):
+            item = self.listWidget_pdbresidues.selectedItems()[0]
+
+            label.setText(item.text())
+
+        self.nmrlabels = []
+        self.pdbassign = []
+        for signal in [f"Res{x}" for x in range(10)]:
+            self.nmrlabels.append(QLabel(parent=self.widget, text=signal))
+            self.verticalLayout.addWidget(self.nmrlabels[-1])
+
+
+            self.pdbassign.append(QLabel(parent=self.widget, text=signal))
+            self.verticalLayout_2.addWidget(self.pdbassign[-1])
+
+            button = QPushButton(self.widget, text ="<" )
+            button.setFixedWidth(20)
+            button.setFixedHeight(17) #todo get the value from the height of the labels
+
+            button.clicked.connect(lambda e, label = self.pdbassign[-1]: change_label(label))
+            self.verticalLayout_3.addWidget(button)
+
+        self.verticalLayout.addItem(QSpacerItem(20, 40, QSizePolicy.MinimumExpanding,QSizePolicy.Expanding))
+        self.verticalLayout_2.addItem(QSpacerItem(20, 40, QSizePolicy.MinimumExpanding,QSizePolicy.Expanding))
+        self.verticalLayout_3.addItem(QSpacerItem(20, 40, QSizePolicy.MinimumExpanding, QSizePolicy.Expanding))
         #
+
         #
