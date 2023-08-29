58a5568a611e2780e3d3725b4a2fe83c03ff9d96
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2023-08-29 15:27:37 +0200

    Added reference to pyDR_tutorial webpage

--- IO/download.py
+++ IO/download.py
@@ -3,6 +3,7 @@
 
 from urllib.request import urlretrieve
 import os
+from warnings import warn
 
 def download(url:str,filename:str='temp'):
     """
@@ -24,6 +25,7 @@ def download(url:str,filename:str='temp'):
     
     if 'drive.google.com' in url:
         return download_google_drive(url,filename)
+        # return download_file_from_google_drive(url, filename)
     
     try:
         out=urlretrieve(url,filename)
@@ -53,6 +55,11 @@ def getPDB(PDBid:str,filename:str=None):
     assert len(PDBid)==4,'The four character PDB id must be used'
     
     if filename is None:filename=PDBid.upper()+'.pdb'
+    
+    if os.path.exists(filename):
+        warn('File already exists')
+        return
+        
     
     url='https://files.rcsb.org/download/{}.pdb'.format(PDBid.upper())
     try:
@@ -84,6 +91,7 @@ def download_google_drive(url:str,filename:str='temp'):
     
     try:
         out=urlretrieve(url,filename)
+        print(out[1])
         return out[0]
     except:
         print('File not found')
@@ -116,3 +124,44 @@ def cleanup_google_link(link:str):
     link=f'https://drive.google.com/uc?id={ID}'
     
     return link
+
+
+import requests
+
+def download_file_from_google_drive(link, destination):
+    print('Updated')
+    
+    a,b=os.path.split(link)
+    if 'view?' in b:
+        link=a
+        ID=os.path.split(link)[1]
+    else:
+        ID=b
+    
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : ID }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : ID, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
